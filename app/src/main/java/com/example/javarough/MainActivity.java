2438f60090854406f494825feeb69a4164c20f9c
vidhanshu37 <kelawalavidhanshu@gmail.com>
2024-07-29 00:12:24 +0530

    neha

--- app/src/main/java/com/example/javarough/MainActivity.java
+++ app/src/main/java/com/example/javarough/MainActivity.java
@@ -8,9 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends AppCompatActivity {
 
 
+    MaterialButton btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btnlogin = findViewById(R.id.btnLogin);
+        btnlogin.setOnClickListener(v-> {
+
         });
     }
 }
